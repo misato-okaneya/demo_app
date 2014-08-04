9c00ecd929b35ed204b7875c0cbbf9622253b7eb
misato-okaneya <misato-okaneya@cookpad.com>
2014-08-04 17:42:48 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '8f4e8d27ec65afb953acec8ab1a9c7c9048fdb45f139a40c75c6d54dc482e2cec27c7603d37cfc369877135d442344281dec080ec59308edcbb0e676688e7ac3'
